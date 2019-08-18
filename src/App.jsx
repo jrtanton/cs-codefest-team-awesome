efc493e6b152e8e701bb83feaed4aa737debcfa7
WilliamConnatser <43946230+WilliamConnatser@users.noreply.github.com>
2019-08-18 05:54:34 -0500

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Redirect, Route } from "react-router-dom";
-import Home from "./components/home/home";
+import LandingPage from "./components/landingPage/landingPage";
 import CreateEvent from "./components/createEvent/createEvent";
 import Register from "./components/register/register";
 import NotFound from "./common/notfound";
@@ -12,7 +12,7 @@ function App() {
       <Switch>
         <Route path={`/create-event`} component={CreateEvent} />
         <Route path={`/register`} component={Register} />
-        <Route path={`/home`} component={Home} />
+        <Route path={`/home`} component={LandingPage} />
         <Route path={`/not-found`} component={NotFound} />
         <Redirect from={`/`} exact to={`/home`} />
         <Redirect to={`/not-found`} />
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <BrowserRouter basename={"/iWealth"}>
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById("tax-lien-calculator")
