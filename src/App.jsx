2504055e3fd8835cb1cd59145ee805332735c413
WilliamConnatser <william@williamconnatser.com>
2019-08-18 00:18:01 -0500

    finished create event form and started on registration component and forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Switch, Redirect, Route } from "react-router-dom";
 import Home from "./components/home/home";
+import CreateEvent from "../createEvent/createEvent";
+import Register from "../register/register";
 import NotFound from "./common/notfound";
 import "./App.css";
 
@@ -8,6 +10,8 @@ function App() {
   return (
     <main className="container">
       <Switch>
+        <Route path={`/create-event`} component={CreateEvent} />
+        <Route path={`/register`} component={Register} />
         <Route path={`/home`} component={Home} />
         <Route path={`/not-found`} component={NotFound} />
         <Redirect from={`/`} exact to={`/home`} />
